a5aab065b21bb16ca6a8b65cc7d9778f1d899214
Emmah Zanele Mpofu <148703284+Zaemah@users.noreply.github.com>
2023-11-23 04:51:02 -0800

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -39,7 +39,7 @@ export default function Weather(props) {
 
   if (weatherData.ready) {
     return (
-      <div className="Weather shadow-lg">
+      <div className="Weather bg-transparent shadow-lg">
         <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
